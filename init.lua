ecd74e6e2cd3739dae08e1d9657ce2723c74e457
Unix Meow <162717024+unixmeow@users.noreply.github.com>
2024-03-12 15:20:31 +0100

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,23 @@
+minetest.register_node("color:pink", {
+    description = "pink",
+    tiles = {
+        "pink.png"
+    },
+groups = {oddly_breakable_by_hand = 3}
+})
+
+minetest.register_node("color:charm", {
+    description = "charm",
+    tiles = {
+        "charm.png"
+    },
+groups = {oddly_breakable_by_hand = 3}
+})
+
+minetest.register_node("color:white", {
+    description = "white",
+    tiles = {
+        "white.png"
+    },
+groups = {oddly_breakable_by_hand = 3}
+})
